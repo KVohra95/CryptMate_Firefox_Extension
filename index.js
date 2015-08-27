046e0b39b9494b161c493459668768aafc64bf1b
Kabeer Vohra <kabeer@vohra.eu>
2015-08-27 11:32:29 +0530

    got insert text working

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ function dummy(text, callback) {
 }
 
 exports.dummy = dummy;
+
+var self = require('sdk/self');
+
+// a dummy function, to show how tests work.
+// to see how to test this function, look at test/test-index.js
+function dummy(text, callback) {
+  callback(text);
+}
+
+exports.dummy = dummy;
+
+var generatedpassword = "";
+
+
+
+var cm = require("sdk/context-menu");
+cm.Item({
+  label: "Insert Password",
+  context: cm.SelectorContext("input[type=password], input[type=text]"),
+  contentScript:  'self.on("click", function (node, data) {' +
+  '  node.value = "' + generatedpassword + '";' +
+  '});'
+});
